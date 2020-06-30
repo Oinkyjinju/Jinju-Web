e12ed0b04be0afb0f9d9b608ce697c055d6e8397
Oinkyjinju <oinkyjinju@gmail.com>
2020-06-30 00:23:50 -0600

    updated img location

--- Popup.js
+++ Popup.js
@@ -1,5 +1,25 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
-function onClick(element) {
-  document.getElementById("img01").src = element.src;
-  document.getElementById("modal01").style.display = "block";
+// Get the button that opens the modal
+var onclick = document.getElementById("popup");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+onclick.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
