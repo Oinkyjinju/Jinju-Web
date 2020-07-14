0899ebcd884e748d49d4feb87c096bcd5cc0955b
Oinkyjinju <oinkyjinju@gmail.com>
2020-07-14 01:27:20 -0600

    updating portfolio  b/n button

--- Popup.js
+++ Popup.js
@@ -11,12 +11,6 @@ var span = document.getElementsByClassName("close")[0];
 
 popup.onclick = function() {
   modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
